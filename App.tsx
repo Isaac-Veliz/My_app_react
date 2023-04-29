d6a2de85631aa746f1467a2dcd77ad7938ab07cb
Isaac-Veliz <83224738+Isaac-Veliz@users.noreply.github.com>
2023-04-29 11:14:48 -0400

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello Anabel!</h1>
+      <p>Como estas :)</p>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# My_app_react
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-paxbd4)
